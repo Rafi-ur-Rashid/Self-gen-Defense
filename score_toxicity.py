dfcb1f626bffceec16a9ca0f08dd8e66a1fdfe08
Rafi-ur-Rashid <rafiurrashid150@gmail.com>
2025-02-19 14:06:59 -0500

    toxicity scoring added

--- score_toxicity.py
+++ score_toxicity.py
@@ -1,4 +1,31 @@
-from transformers import pipeline
-toxicity_classifier = pipeline("text-classification", model="unitary/toxic-bert")
-result = toxicity_classifier("This is a toxic statement.")
-print(result)
+from detoxify import Detoxify
+from accelerate import Accelerator
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from tqdm import tqdm
+import pandas as pd
+
+import json
+
+
+model = Detoxify('original')
+
+json_file="out/toxicity_gpt-4o_response.json"
+
+# Load the JSON file
+with open(json_file, "r", encoding="utf-8") as file:
+    questions = json.load(file)  # Load JSON as list of dictionaries
+
+output = []
+
+for question in questions:
+
+    q = question['response']
+    outputs = model.predict(q)
+    
+    response = {key: float(value) for key, value in outputs.items()}
+    question['toxic-bert-score']=response
+    
+    # print(q)
+
+with open(json_file, 'w') as f:
+        json.dump(questions, f, indent=4)
